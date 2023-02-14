409eb8b4ea086d67b4fa6c2aceabfcd6b3018319
Allen Zhang <allenjameszhang@gmail.com>
2023-02-14 13:25:29 -0500

    rm render_images

--- render_images_macos.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -e blender ]; then 
-    echo "Blender not found. Please run download_blender.sh first."
-    exit 1
-fi
-(
-    cd image_generation \
-    && \
-    ../blender/blender.app/Contents/MacOS/blender -noaudio --background --python render_images.py -- --num_images 1 --min_objects 2 --max_objects 2 --render_num_samples 128 --render_tile_size 32
-)
